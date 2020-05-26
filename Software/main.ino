bbf6274b8ce5daeb6c71f023b47d51de2a43513a
Florian <florian@brandner.net>
2020-05-26 12:53:10 +0200

    Changed bignums to 3x3

--- Software/main.ino
+++ Software/main.ino
@@ -7,6 +7,7 @@
  * 
  * Remarks:
  * - The 40x4 is technically a 80x2, so setCursor and line overflow won't work as expected
+ *   Line 3 is basically the second half of line 1, and line 4 is the second half of line 2
  * 
  * LCD Connections:
  *  8 .. RS
@@ -22,256 +23,174 @@
 #include <RTClib.h>
 #include <Adafruit_BME280.h>
 
-static char         daysOfTheWeek[7][3] = {"So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"};
+/*******************************************************
+ * Defines
+ *******************************************************/
+#define             LCD_CUSTCHARS           8               //< The amount of custom chars for the LCD
+#define             BME_RATE                60              //< The BME is updated every 60 secs
+#define             LCD_LINEOFFSET          20              //< The offset to get into Line 3 and 4
 
-// RTC
-RTC_DS1307          rtc;
-DateTime            rtc_now;
-// LCD
-// the 8 arrays that form each segment of the custom numbers
-byte bar1[8] = { B11100, B11110, B11110, B11110, B11110, B11110, B11110, B11100 };
-byte bar2[8] = { B00111, B01111, B01111, B01111, B01111, B01111, B01111, B00111 };
-byte bar3[8] = { B11111, B11111, B00000, B00000, B00000, B00000, B11111, B11111 };
-byte bar4[8] = { B11110, B11100, B00000, B00000, B00000, B00000, B11000, B11100 };
-byte bar5[8] = { B01111, B00111, B00000, B00000, B00000, B00000, B00011, B00111 };
-byte bar6[8] = { B00000, B00000, B00000, B00000, B00000, B00000, B11111, B11111 };
-byte bar7[8] = { B00000, B00000, B00000, B00000, B00000, B00000, B00111, B01111 };
-byte bar8[8] = { B11111, B11111, B00000, B00000, B00000, B00000, B00000, B00000 };
+/*******************************************************
+ * Statics
+ *******************************************************/
+static char         lcd_DoW[7][3] = {"So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"}; //< The days for the LCD
 
-hd44780_pinIO       lcd(8, 9, 4, 5, 6, 7);
-uint32_t            lcd_update = 0;               // Last LCD update
-//BME
-Adafruit_BME280     bme;
-uint32_t            bme_update = 0;               // Last reading
-float               bme_temp = 0.0;
-float               bme_pres = 0.0;
-float               bme_humi = 0.0;
-#define             BME_RATE            60          // BME is updated every 60 secs
+static byte CustChars[LCD_CUSTCHARS][8] = {                 // The custom chars to create the big numbers
+  { 0x03, 0x07, 0x0F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F },
+  { 0x18, 0x1C, 0x1E, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F },
+  { 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F },
+  { 0x1F, 0x0F, 0x07, 0x03, 0x00, 0x00, 0x00, 0x00 },
+  { 0x1F, 0x1E, 0x1C, 0x18, 0x00, 0x00, 0x00, 0x00 },
+  { 0x1F, 0x1F, 0x1F, 0x1F, 0x00, 0x00, 0x00, 0x00 },
+  { 0x03, 0x07, 0x0F, 0x1F, 0x00, 0x00, 0x00, 0x00 },
+  { 0x0E, 0x0A, 0x0E, 0x00, 0x00, 0x0E, 0x0A, 0x0E }
+};
+
+static uint8_t CustNumbers[10][9] = {                       // The segments of the numbers 0..9
+  { 0, 5, 1,    2, 32, 2,   3, 5, 4 }, // 0
+  { 6, 2, 32,   32, 2, 32,  6, 5, 4 }, // 1
+  { 6, 5, 1,    0, 5, 4,    5, 5, 4 }, // 2
+  { 6, 5, 1,    32, 5, 2,   3, 5, 4 }, // 3
+  { 2, 32, 2,   3, 5, 2,    32,32,5 }, // 4
+  { 2, 5, 4,    5, 5, 1,    3, 5, 4 }, // 5
+  { 0, 5, 4,    2, 5, 1,    3, 5, 4 }, // 6
+  { 3, 5, 1,    32,32,2,    32,32,5 }, // 7
+  { 0, 5, 1,    2, 5, 2,    3, 5, 4 }, // 8
+  { 0, 5, 1,    3, 5, 2,    3, 5, 4 }  // 9
+};
+
+/*******************************************************
+ * Local variables
+ *******************************************************/
+RTC_DS1307          rtc;                                    //< The RTC object
+hd44780_pinIO       lcd(8, 9, 4, 5, 6, 7);                  //< The LCD object
+Adafruit_BME280     bme;                                    //< The BME object
+DateTime            time_now;                               //< The current time
+
+uint32_t            lcd_update = 0;                         //< Timestamp of the last LCD update
+uint32_t            bme_update = 0;                         //< Timestamp of the last BME reading
+float               bme_temp = 0.0;                         //< The current temperature
+float               bme_pres = 0.0;                         //< The current pressure
+float               bme_humi = 0.0;                         //< The current humidity
 
 
 /*******************************************************
  * RTC
  *******************************************************/
+/********************************* Setup */
 void setup_rtc() {
     // Init RTC
     if (! rtc.begin()) {
-        Serial.println("Couldn't find RTC");
+        Serial.println(F("[RTC] Couldn't find RTC"));
         lcd.print("Error: RTC!");
         while (1); // loop forever
     }
     if (!rtc.isrunning()) {
-        Serial.println("RTC is NOT running!");
+        Serial.println(F("[RTC] RTC is NOT running!"));
+        // Adjust to build date/time
         rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     }
     rtc.writeSqwPinMode(DS1307_OFF); // No Sqw-Output
 } // setup_rtc
-
 /*******************************************************
  * LCD
  *******************************************************/
-void custom0(int col)
-{ // uses segments to build the number 0
-  lcd.setCursor(col, 0); 
-  lcd.write(2);  
-  lcd.write(8); 
-  lcd.write(1);
-  lcd.setCursor(col, 1); 
-  lcd.write(2);  
-  lcd.write(6);  
-  lcd.write(1);
-}
+/********************************* Print a Custom Number */
+void lcd_printdigit(uint8_t Col, uint8_t Digit) {
+  if (Digit>9) return;  // boundary check: Valid digits are 0..9
+  if (Col>17) return;   // boundary check: only columns 0..17 are valid for this display
 
-void custom1(int col)
-{
-  lcd.setCursor(col,0);
-  lcd.write(32);
-  lcd.write(32);
-  lcd.write(1);
-  lcd.setCursor(col,1);
-  lcd.write(32);
-  lcd.write(32);
-  lcd.write(1);
-}
+  lcd.setCursor(Col,0);
+  lcd.write(CustNumbers[Digit][0]);
+  lcd.write(CustNumbers[Digit][1]);
+  lcd.write(CustNumbers[Digit][2]);
+  lcd.setCursor(Col,1);
+  lcd.write(CustNumbers[Digit][3]);
+  lcd.write(CustNumbers[Digit][4]);
+  lcd.write(CustNumbers[Digit][5]);
+  lcd.setCursor(Col+LCD_LINEOFFSET,0);
+  lcd.write(CustNumbers[Digit][6]);
+  lcd.write(CustNumbers[Digit][7]);
+  lcd.write(CustNumbers[Digit][8]);
 
-void custom2(int col)
-{
-  lcd.setCursor(col,0);
-  lcd.write(5);
-  lcd.write(3);
-  lcd.write(1);
-  lcd.setCursor(col, 1);
-  lcd.write(2);
-  lcd.write(6);
-  lcd.write(6);
-}
-
-void custom3(int col)
-{
-  lcd.setCursor(col,0);
-  lcd.write(5);
-  lcd.write(3);
-  lcd.write(1);
-  lcd.setCursor(col, 1);
-  lcd.write(7);
-  lcd.write(6);
-  lcd.write(1); 
-}
-
-void custom4(int col)
-{
-  lcd.setCursor(col,0);
-  lcd.write(2);
-  lcd.write(6);
-  lcd.write(1);
-  lcd.setCursor(col, 1);
-  lcd.write(32);
-  lcd.write(32);
-  lcd.write(1);
-}
-
-void custom5(int col)
-{
-  lcd.setCursor(col,0);
-  lcd.write(2);
-  lcd.write(3);
-  lcd.write(4);
-  lcd.setCursor(col, 1);
-  lcd.write(7);
-  lcd.write(6);
-  lcd.write(1);
-}
-
-void custom6(int col)
-{
-  lcd.setCursor(col,0);
-  lcd.write(2);
-  lcd.write(3);
-  lcd.write(4);
-  lcd.setCursor(col, 1);
-  lcd.write(2);
-  lcd.write(6);
-  lcd.write(1);
-}
-
-void custom7(int col)
-{
-  lcd.setCursor(col,0);
-  lcd.write(2);
-  lcd.write(8);
-  lcd.write(1);
-  lcd.setCursor(col, 1);
-  lcd.write(32);
-  lcd.write(32);
-  lcd.write(1);
-}
-
-void custom8(int col)
-{
-  lcd.setCursor(col, 0); 
-  lcd.write(2);  
-  lcd.write(3); 
-  lcd.write(1);
-  lcd.setCursor(col, 1); 
-  lcd.write(2);  
-  lcd.write(6);  
-  lcd.write(1);
-}
-
-void custom9(int col)
-{
-  lcd.setCursor(col, 0); 
-  lcd.write(2);  
-  lcd.write(3); 
-  lcd.write(1);
-  lcd.setCursor(col, 1); 
-  lcd.write(7);  
-  lcd.write(6);  
-  lcd.write(1);
-}
-void setup_lcd() {
-    lcd.begin(80, 2);
-
-	lcd.createChar(1, bar1);
-    lcd.createChar(2, bar2);
-	lcd.createChar(3, bar3);
-	lcd.createChar(4, bar4);
-	lcd.createChar(5, bar5);
-	lcd.createChar(6, bar6);
-	lcd.createChar(7, bar7);
-	lcd.createChar(8, bar8);
-
-    lcd.setCursor(0,0);
-
-} // setup_lcd
-void lcd_printdigit(uint8_t Val, uint8_t Col) {
-    switch (Val) {
-        case 0: custom0(Col); break;
-        case 1: custom1(Col); break;
-        case 2: custom2(Col); break;
-        case 3: custom3(Col); break;
-        case 4: custom4(Col); break;
-        case 5: custom5(Col); break;
-        case 6: custom6(Col); break;
-        case 7: custom7(Col); break;
-        case 8: custom8(Col); break;
-        case 9: custom9(Col); break;
-
-    }
+  return;
 } // lcd_printdigit
-
+/********************************* Setup */
+void setup_lcd() {
+  // Init lcd object and create custom chars
+  lcd.begin(80, 2);
+  for (uint8_t i=0;i<LCD_CUSTCHARS; i++) lcd.createChar(i, CustChars[i]);
+} // setup_lcd
+/********************************* Worker */
 void lcd_worker() {
-    char cBuffer[40];
-    uint8_t Digit1=0;
-    uint8_t Digit2=0;
+  static uint8_t    dispmode=0;
+  char              cBuffer[20];
+  uint8_t           Digit1=0;
+  uint8_t           Digit2=0;
 
+  if (time_now.secondstime() == lcd_update) return; // Not yet
+  lcd_update = time_now.secondstime();
 
-    if (rtc_now.secondstime() == lcd_update) return; // Not yet
-    lcd_update = rtc_now.secondstime();
+  // Display mode: switch between temp, pressure, humidity every 5 secs
+  if (lcd_update%5==0) {
+    dispmode++;
+    if (dispmode>2) dispmode=0;
+  }
 
-    lcd.clear();
+//  lcd.clear(); 
+  lcd.setCursor(0,0);
 
-    // Time
-    Digit1 = rtc_now.hour()/10;
-    Digit2 = rtc_now.hour() - (10*Digit1);
-    lcd_printdigit(Digit1, 0);
-    lcd_printdigit(Digit2, 3);
+  // Time
+  Digit1 = time_now.hour()/10;
+  Digit2 = time_now.hour() - (10*Digit1);
+  lcd_printdigit(0, Digit1);
+  lcd_printdigit(3, Digit2);
 
-    lcd.setCursor(6,0);     lcd.print("o");
-    lcd.setCursor(6,1);     lcd.print("o");
+  lcd.setCursor(6,1);     lcd.write(7);
 
-    Digit1 = rtc_now.minute()/10;
-    Digit2 = rtc_now.minute() - (10*Digit1);
-    lcd_printdigit(Digit1, 7);
-    lcd_printdigit(Digit2, 10);
+  Digit1 = time_now.minute()/10;
+  Digit2 = time_now.minute() - (10*Digit1);
+  lcd_printdigit( 7, Digit1);
+  lcd_printdigit(10, Digit2);
 
-    lcd.setCursor(13,0);     lcd.print("o");
-    lcd.setCursor(13,1);     lcd.print("o");
+  lcd.setCursor(13,1);     lcd.write(7);
 
-    Digit1 = rtc_now.second()/10;
-    Digit2 = rtc_now.second() - (10*Digit1);
-    lcd_printdigit(Digit1, 14);
-    lcd_printdigit(Digit2, 17);
+  Digit1 = time_now.second()/10;
+  Digit2 = time_now.second() - (10*Digit1);
+  lcd_printdigit(14, Digit1);
+  lcd_printdigit(17, Digit2);
 
-    // Date
-    snprintf(&cBuffer[0],40,"%s, %02d.%02d.%02d",  daysOfTheWeek[rtc_now.dayOfTheWeek()], rtc_now.day(), rtc_now.month(), rtc_now.year()  );
-    lcd.setCursor(26,0);
-    lcd.print(cBuffer);
+  // Date
+  snprintf(&cBuffer[0],20,"%s,%02d.%02d.%02d",  lcd_DoW[time_now.dayOfTheWeek()], time_now.day(), time_now.month(), time_now.year()-2000  );
 
-    // Temperature
-    snprintf(&cBuffer[0],40,"%d.%dC %dhPa %d%%", (uint8_t)bme_temp, (uint16_t)(10*bme_temp)-(((uint16_t)bme_temp)*10), (uint16_t)bme_pres, (uint8_t)bme_humi );
+  lcd.setCursor(LCD_LINEOFFSET,1); // Start of Line 4
+  for (uint8_t i=0;i<(20-strlen(cBuffer));i++) lcd.print(" "); // print spaces to overwrite old stuff and make date right-align
+  lcd.print(cBuffer);
 
-    lcd.setCursor(20,1);
-    if (strlen(cBuffer) < 20) for (uint8_t i=0;i<20-strlen(cBuffer);i++) lcd.print(" "); // right-align
-    lcd.print(cBuffer);
-
-
+  switch (dispmode) {
+    case 0: // Temperature
+      snprintf(&cBuffer[0],20,"%d.%dC", (uint8_t)bme_temp, (uint16_t)(10*bme_temp)-(((uint16_t)bme_temp)*10) );
+      break;
+    case 1:
+      snprintf(&cBuffer[0],20,"%dhP", (uint16_t)bme_pres );
+      break;
+    case 2:
+      snprintf(&cBuffer[0],20,"%d%%", (uint16_t)bme_humi );
+      break;
+    default:
+      dispmode=0;
+      break;
+  }
+  lcd.setCursor(LCD_LINEOFFSET,1); // Line 4
+  lcd.print(cBuffer);
 } // lcd_worker
 /*******************************************************
  * BME
  *******************************************************/
+/********************************* Setup */
 void setup_bme() {
     if (!bme.begin(0x76)) {
-        Serial.println("Could not find a valid BME280 sensor, check wiring!");
+        Serial.println(F("[BME] Could not find a valid BME280 sensor, check wiring!"));
         while (1);
     }
     bme.setSampling(Adafruit_BME280::MODE_FORCED,
@@ -281,11 +200,12 @@ void setup_bme() {
                     Adafruit_BME280::FILTER_OFF   );
     
 } // setup_bme
+/********************************* Worker */
 void bme_worker() {
-    if (rtc_now.secondstime() <= bme_update+BME_RATE) return;   // Update time not yet reached
+    if (time_now.secondstime() <= bme_update+BME_RATE) return;   // Update time not yet reached
 
-    bme_update = rtc_now.secondstime();
-    Serial.print("[BME] Updateing ");
+    bme_update = time_now.secondstime();
+    Serial.print(F("[BME] Updateing "));
 
     bme.takeForcedMeasurement();
     bme_temp = bme.readTemperature();
@@ -303,40 +223,27 @@ void bme_worker() {
 
 } // bme_worker
 /*******************************************************
- * Setup
- *******************************************************/
-void setup() {
-    Serial.begin(115200);
-    Serial.println("[SYS] Build: " __DATE__ " - " __TIME__);
-    Serial.println("[SYS] Starting Setup...");
-
-    setup_lcd();
-    setup_rtc();
-    setup_bme();
-
-    Serial.println("[SYS] Setup complete");
-}
-/*******************************************************
  * Aux-Functions
  *******************************************************/
+/********************************* Startup Info */
 void start_info() {
     uint8_t buffer[56];
     
     // Show Build-Info
-    Serial.println("Build: " __DATE__ " " __TIME__ );
+    Serial.println(F("Build: " __DATE__ " " __TIME__ ));
     lcd.clear();
     lcd.setCursor(0,0);
-    lcd.print("Build: ");
+    lcd.print(F("Build: "));
     lcd.setCursor(0,1);
-    lcd.print(__DATE__);
+    lcd.print(F(__DATE__));
     lcd.setCursor(20,0);
-    lcd.print(__TIME__);
+    lcd.print(F(__TIME__));
     delay(1000);  
 
     // Print out DS1307s NVRAM
     rtc.readnvram(&buffer[0],56,0);
 
-    Serial.print("[RTC] NVRAM:");
+    Serial.print(F("[RTC] NVRAM:"));
     for (uint8_t i=0;i<56;i++) {
         if (i%8==0) Serial.println();
         Serial.print(" ");
@@ -347,15 +254,28 @@ void start_info() {
     lcd.clear();
 } // start_info
 /*******************************************************
- * Loop
+ * Main stuff
  *******************************************************/
+/********************************* Setup */
+void setup() {
+    Serial.begin(115200);
+    Serial.println(F("[SYS] Build: " __DATE__ " - " __TIME__));
+    Serial.println(F("[SYS] Starting Setup..."));
+
+    setup_lcd();
+    setup_rtc();
+    setup_bme();
+
+    Serial.println(F("[SYS] Setup complete"));
+}
+/********************************* Loop */
 void loop() {
-    Serial.println("[SYS] Entering Loop!");
+    Serial.println(F("[SYS] Entering Loop!"));
 
     start_info();
 
     while (1) {
-        rtc_now = rtc.now();
+        time_now = rtc.now();
 
         bme_worker();
         lcd_worker();
